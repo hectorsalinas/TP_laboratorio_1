cdad908996b9af013bcfaa6ab3b21d2299837503
hectorsalinas <hsscazador@yahoo.com.ar>
2017-04-06 20:47:35 -0300

    HectorSalinas
    Subida del trabajo practico

---
+++ funciones.h
@@ -0,0 +1,16 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+#endif // FUNCIONES_H_INCLUDED
+
+int ValEntero(int Variable, int VariableMin, int VariableMax);
+void Divicion(int VariableA, int VariableB);
+void Multiplicacion(int VariableA, int VariableB);
+void Resta(int VariableA, int VariableB);
+void Suma(int VariableA, int VariableB);
+void Factorial(int varF);
+int ValIntCero(int VariableB);
+int FactorialDeClase(int num);//es referencia a la que se mostro como ejemplo en clase de laboratorio
+
+
+
